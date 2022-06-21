347bea2bc55f734cd6ba5286761728c243caa935
unknown <andynguyenvn1103@gmail.com>
2022-06-22 00:57:28 +0700

    finish login and registration function

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\UserType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class RegistrationController extends AbstractController
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function index(Request $request): Response
+    {
+        $user=new User();
+        $form=$this->createForm(UserType::class,$user);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()&&$form->isValid())
+        {
+            //set role
+            $user->setRoles(['ROLE_USER']);
+            //hash the plain password before writing to the db
+            $user->setPassword(
+                $this->passwordEncoder->encodePassword($user,$user->getPassword())
+            );
+            //save to the db
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            return $this->redirectToRoute('app_login');
+
+        }
+        return $this->render('registration/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
