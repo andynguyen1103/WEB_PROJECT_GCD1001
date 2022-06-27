91edef1647ec832a2423064c3d37b4092bc4e24f
IriS <mhuynk1005@gmail.com>
2022-06-27 16:44:23 +0700

    Add javascript and fix a little for footer

---
+++ public/js/app.js
@@ -0,0 +1,2 @@
+document.querySelector('#mb-menu-toggle').addEventListener('click', () => document.querySelector('#header-wrapper').classList.add('active'))
+document.querySelector('#mb-menu-close').addEventListener('click', () => document.querySelector('#header-wrapper').classList.remove('active'))
