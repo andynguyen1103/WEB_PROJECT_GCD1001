354fc42d34a4d9c8910b129de33b4de5aa85cb98
voanhtu3010 <voanhtu3010@gmail.com>
2022-06-28 13:18:22 +0700

    Add function to order controller

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,19 +2,101 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
     /**
-     * @Route("/order", name="app_order")
+     * @Route("/order",name="order_list")
      */
-    public function index(): Response
+    public function listAction() {
+        $orders = $this->getDoctrine()
+            ->getRepository(Order::class)
+            ->findAll();
+        return $this->render('order/index.html.twig',['orders'=>$orders]);
+    }
+
+    /**
+     * @Route("/order/details/{id}",name="order_details")
+     */
+    public function detailsAction($id) {
+        $orders = $this->getDoctrine()
+            ->getRepository(Order::class)
+            ->find($id);
+        return $this->render('order/details.html.twig',['orders'=>$orders]);
+    }
+
+    /**
+     * @Route("/order/delete/{id}",name="order_delete")
+     */
+    public function deleteAction($id) {
+        $em = $this->getDoctrine()->getManager();
+        $order = $em->getRepository(Order::class)->find($id);
+        $em->remove($order);
+        $em->flush();
+
+        $this->addFlash('error','Order deleted');
+        return $this->redirectToRoute('order_list');
+    }
+
+    /**
+     * @Route("/todo/create", name="todo_create", methods={"GET","POST"})
+     */
+    public function createAction(Request $request)
     {
-        return $this->render('order/index.html.twig', [
-            'controller_name' => 'OrderController',
+        $order = new Order();
+        $form = $this->createForm(Order::class, $order);
+
+        if ($this->saveChanges($form, $request, $order)) {
+            $this->addFlash(
+                'notice',
+                'Order Added'
+            );
+
+            return $this->redirectToRoute('order_list');
+        }
+
+        return $this->render('order/create.html.twig', [
+            'form' => $form->createView()
         ]);
+    }
+
+    public function saveChanges($form, $request, $order)
+    {
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $order->setUser($request->request->get('order')['user']);
+            $order->setProduct($request->request->get('order')['product']);
+            $order->setVendor($request->request->get('order')['vendor']);
+            $order->setAmount($request->request->get('order')['amount']);
+            $order->setOrderDate(\DateTime::createFromFormat('Y-m-d', $request->request->get('order')['order_date']));
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($order);
+            $em->flush();
+
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @Route("/todo/edit/{id}", name="todo_edit")
+     */
+    public function editAction($id, Request $request) {
+        $em = $this->getDoctrine()->getManager();
+        $order = $em->getRepository(Order::class)->find($id);
+        $form = $this->createForm(Order::class,$order);
+
+        if ($this->saveChanges($form, $request, $order)) {
+            $this->addFlash('notice', "Order Edited");
+            return $this->redirectToRoute('order_list');
+        }
+
+        return $this->render('order/edit.html.twig', ['form'=>$form->createView()]);
     }
 }
